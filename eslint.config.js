f3498eae7820758e43870026c3454cfa4e4bd9ee
gomDarachi <ttcoristory@naver.com>
2025-06-24 02:23:48 +0900

    chore: eslint-config-prettier 설치 및 ESLint 설정에 prettier 확장 추가
    - eslint-config-prettier 패키지 개발 의존성으로 설치
    - ESLint extends에 "prettier" 추가하여 Prettier와 ESLint 충돌 방지

--- eslint.config.js
+++ eslint.config.js
@@ -8,7 +8,7 @@ export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
     plugins: { js },
-    extends: ["js/recommended"],
+    extends: ["js/recommended", "prettier"],
   },
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
