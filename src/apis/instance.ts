3c97b07ea63da7fd5d9314fefde682074b0a51e5
gomDarachi <ttcoristory@naver.com>
2025-06-24 18:31:52 +0900

    feat: axios 인스턴스에 인증 및 에러 처리를 위한 인터셉터 추가

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -13,4 +13,27 @@ const instance = axios.create({
   timeout: API_TIMEOUT,
 });
 
+// 요청 인터셉터: 인증 토큰 추가
+instance.interceptors.request.use(
+  (config) => {
+    // const token = getAuthToken();
+    // if (token) {
+    //   config.headers.Authorization = `Bearer ${token}`;
+    // }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+// 응답 인터셉터: 에러 처리 등
+instance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    // if (error.response?.status === 401) {
+    //   // 로그아웃 처리, 토큰 갱신, 경고창 등
+    // }
+    return Promise.reject(error);
+  }
+);
+
 export default instance;
