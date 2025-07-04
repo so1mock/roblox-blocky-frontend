2cf49fc256617fa01651c9a74cb214bfa031709d
bestoon <zzzz6747@naver.com>
2025-07-04 16:04:53 +0900

    css 수정, 외관 정상화

--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -2,10 +2,10 @@ import { HeaderButton } from "./HeaderButton";
 
 export function Header() {
   return (
-    <header className="flex flex-row bg-[#9D7CDE] text-white items-center p-2 justify-between">
+    <header className="flex flex-row bg-[#9D7CDE] h-12">
       <img src="/LuaBlock_logo.png" />
 
-      <nav className="flex flex-row p-2 space-x-4 mx-auto">
+      <nav className="flex-1 flex justify-center p-2 space-x-4 ">
         {/* 페이지 이름은 아직 완벽히 정하지 않음 */}
         <HeaderButton to="/project" name="만들기" />
         <HeaderButton to="/tutorial" name="튜토리얼" />
