e6956c3ec2ff120fa74434d8b4e8fddc1ce3bbc5
gomDarachi <ttcoristory@naver.com>
2025-07-09 21:32:52 +0900

    fix: 필요없는 hook 선언 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { useTheme } from "./common/hooks/useTheme";
 
 function App() {
-  const [theme, setTheme] = useTheme();
-
   return (
     <>
       <div>
