00e7fbcf7355b28e13cde428dffd4e505d01d4bf
bestoon <zzzz6747@naver.com>
2025-07-11 17:32:49 +0900

    fix: Header 배치 수정 nav 버튼들의 gap 증가 소셜 로그인 버튼을 오른쪽으로 정렬 로고, 소셜 로그인 버튼을 absolute로 설정

--- src/domain/common/components/header/LinkNavButton.tsx
+++ src/domain/common/components/header/LinkNavButton.tsx
@@ -2,11 +2,8 @@ import { Link } from "@tanstack/react-router";
 
 export function LinkNavButton({ to, title }: { to: string; title: string }) {
   return (
-    <Link
-      to={to}
-      className="text-white flex items-center whitespace-nowrap h-10"
-    >
-      {title}
-    </Link>
+    <div className="text-white flex items-center whitespace-nowrap h-10 w-15 justify-center">
+      <Link to={to}>{title}</Link>
+    </div>
   );
 }
