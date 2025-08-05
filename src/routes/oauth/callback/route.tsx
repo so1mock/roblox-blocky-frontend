6758640d0b93037a7b6b9c4224964c4d3a782be2
darachi <ttcoristory@naver.com>
2025-08-06 00:47:45 +0900

    fix: 최초 로그인 로직 변경 - accessToken을 header에 담기 - roblox error 처리 로직 작성

--- src/routes/oauth/callback/route.tsx
+++ src/routes/oauth/callback/route.tsx
@@ -9,6 +9,7 @@ import {
   type GetTokenResponse,
 } from "../../../domain/user/apis/getToken";
 import { useEffect } from "react";
+import { api } from "../../../domain/common/apis/axios";
 
 type SearchParams = {
   code?: string;
@@ -35,19 +36,21 @@ export function RouteComponent() {
     mutationFn: getToken,
     onSuccess: (data: GetTokenResponse) => {
       console.log("토큰 발급 성공" + JSON.stringify(data));
-
-      localStorage.setItem("access_token", data.auth.accessToken);
+      api.defaults.headers.common["Authorization"] = data.auth.accessToken;
       localStorage.setItem("user_info", JSON.stringify(data.info));
 
       navigate({ to: "/" });
     },
     onError: (error: Error) => {
-      alert("Mutation 에러" + error);
+      alert("서비스 사용자 인증 에러" + error);
     },
   });
 
   useEffect(() => {
-    if (code) {
+    if (error) {
+      alert("로블록스 로그인에서 에러 발생: " + error.message);
+      navigate({ to: "/" });
+    } else if (code) {
       handleGetToken(code);
     }
   }, []);
