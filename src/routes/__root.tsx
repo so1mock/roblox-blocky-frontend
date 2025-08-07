e03b9423a021e06dc49e9f31883a99123655855a
gomDarachi <ttcoristory@naver.com>
2025-08-07 21:50:56 +0900

    feat: tanstack route의 레이아웃 구현 - MainLayout을 따로 분리 - Project 페이지 분리 - 렌딩페이지 분리

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,5 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
-import { Header } from "../domain/common/components/header/Header";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
@@ -10,10 +9,7 @@ export const Route = createRootRoute({
     return (
       <QueryClientProvider client={queryClient}>
         <div className="min-h-screen flex flex-col">
-          <Header />
-          <main className="bg-rbBg flex-1 flex items-center justify-center">
-            <Outlet />
-          </main>
+          <Outlet />
           <TanStackRouterDevtools />
         </div>
       </QueryClientProvider>
