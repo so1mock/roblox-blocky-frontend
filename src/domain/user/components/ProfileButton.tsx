0038f1644d7fe965c6292a35f373b6d6175e9266
bestoon <zzzz6747@naver.com>
2025-08-13 21:13:11 +0900

    fix: 프로필 버튼 태그 수정

--- src/domain/user/components/ProfileButton.tsx
+++ src/domain/user/components/ProfileButton.tsx
@@ -9,7 +9,7 @@ export function ProfileButton({
 }) {
   const { handleLogout } = useUser();
   return (
-    <a
+    <button
       className={`flex items-center whitespace-nowrap gap-2 h-10 bg-rbLogin min-w-fit p-2 rounded-lg ${className}`}
       onClick={() => {
         handleLogout();
@@ -17,6 +17,6 @@ export function ProfileButton({
     >
       <img src="/profileIcon.png" className="w-7 h-7" />
       <div className="text-white">{nickname} 로그아웃</div>
-    </a>
+    </button>
   );
 }
