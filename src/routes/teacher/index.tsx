bf8096d4847f78e800b29be825dd5f53367d8678
gomDarachi <ttcoristory@naver.com>
2025-09-27 01:59:39 +0900

    feat: footer 구현

--- src/routes/teacher/index.tsx
+++ src/routes/teacher/index.tsx
@@ -8,9 +8,43 @@ export const Route = createFileRoute("/teacher/")({
 
 function RouteComponent() {
   return (
-    <div className="min-h-screen flex flex-col justify-between bg-[url(/mainBackground.png)] bg-center bg-cover">
+    <div className="relative min-h-screen flex flex-col justify-between bg-[url(/mainBackground.png)] bg-center bg-cover">
       <TeacherHeader />
-      <main>메인페이지</main>
+
+      <main className="flex justify-center items-center gap-20 min-w-[1400px]">
+        {/* 텍스트 영역 */}
+        <div className="flex flex-col text-4xl">
+          <span className="mb-2">CoBlocks와 함께, 복잡한 코딩 수업도</span>
+          <span className="font-bold mt-2">
+            쉽고 재미있게 진행할 수 있어요!
+          </span>
+        </div>
+
+        {/* 영상 영역 */}
+        <section className="flex flex-col items-center gap-4">
+          <span className="bg-rbBg text-blue-400 px-8 py-2 rounded-3xl font-bold">
+            튜토리얼
+          </span>
+
+          <video
+            className="rounded-xl shadow-lg w-[640px] border-solid border-8 border-black"
+            controls
+          >
+            <source
+              src="https://www.w3schools.com/html/mov_bbb.mp4"
+              type="video/mp4"
+            />
+            브라우저가 video 태그를 지원하지 않습니다.
+          </video>
+        </section>
+        <div className="absolute bottom-26 bg-rbTextPadding w-full text-center py-2 animate-[marquee] overflow-x-hidden">
+          <span className="text-white text-lg font-bold animate-marquee">
+            로블록스 수업, 어렵지 않아요! 블록을 쌓듯 간단하게 가르치고,학생들의
+            창의력과 협업을 이끌어보세요.
+          </span>
+        </div>
+      </main>
+
       <Footer />
     </div>
   );
