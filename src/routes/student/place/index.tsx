26d8419db7d6b9fa72a5ab12ea439d46db83b3e9
bestoon <zzzz6747@naver.com>
2025-10-04 22:09:41 +0900

    fix: student/place 내용 삭제 아직 구현되지 않은 페이지인데 merge 과정에 남아 있던 잘못된 코드 삭제

--- src/routes/student/place/index.tsx
+++ src/routes/student/place/index.tsx
@@ -1,9 +1,5 @@
-import { Header } from "@common/components/header/Header";
 import { createFileRoute } from "@tanstack/react-router";
 import { verifyAuth } from "@user/utils/authGuard";
-import { useEffect, useState } from "react";
-import { getMyPlaces } from "src/domain/myPlaces/workspace/apis/workspace";
-import type { PlaceSummary } from "src/domain/myPlaces/workspace/types/workspace";
 
 export const Route = createFileRoute("/student/place/")({
   beforeLoad: async () => {
@@ -13,32 +9,5 @@ export const Route = createFileRoute("/student/place/")({
 });
 
 function RouteComponent() {
-  const [myPlaces, setMyPlaces] = useState<PlaceSummary[]>([]);
-
-  useEffect(() => {
-    const fetchMyPlaces = async () => {
-      const response = await getMyPlaces();
-      setMyPlaces(response);
-    };
-    fetchMyPlaces();
-  }, []);
-  return (
-    <>
-      <Header />
-      <div>
-        {myPlaces.map((placeSummary) => {
-          return (
-            <div>
-              {placeSummary.uuid} <br />
-              name: {placeSummary.name} <br />
-              description: {placeSummary.description} <br />
-              owner: {placeSummary.ownerName} <br />
-              lastModified: {placeSummary.lastModifiedAt} <br />
-              <hr />
-            </div>
-          );
-        })}
-      </div>
-    </>
-  );
+  return <div>place id가 있어야 합니다.</div>;
 }
