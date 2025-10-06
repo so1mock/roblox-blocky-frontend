a763215666b43d3796eb2d6270c8f102e2895545
gomDarachi <ttcoristory@naver.com>
2025-10-06 22:18:45 +0900

    feat: 새 플레이스 연결 버튼 UI 구현

--- src/domain/myPlace/components/MyPlacePage.tsx
+++ src/domain/myPlace/components/MyPlacePage.tsx
@@ -43,45 +43,63 @@ function MyPlacePage() {
           <h1 className="text-left">
             <span className="text-3xl font-bold">마이 플레이스</span>
           </h1>
-          <div className="relative w-[180px]">
-            {/* 선택된 옵션 버튼 */}
-            <button
-              className="w-full px-4 py-3 bg-white rounded-2xl cursor-pointer flex justify-between items-center"
-              onClick={() => setIsSearchOptionsOpen((prev) => !prev)}
-            >
-              <span>{selectedSort.name}</span>
-              <div>
-                <img src="/dropdown.png" className="w-4" />
-              </div>
+          <div className="flex gap-4">
+            {/* ℹ️ 안내 버튼 */}
+            <button className="flex items-center gap-2 px-4 py-2 rounded-full border border-[#61C1FD] hover:bg-[#E9F6FF] font-medium transition-all shadow-sm cursor-pointer">
+              <img
+                src="/infoIcon.png"
+                alt="새 플레이스 연결"
+                className="w-5 h-5"
+              />
+              <span className="font-bold text-[#61C1FD] ">
+                새 플레이스 연결
+              </span>
             </button>
+            <div className="relative w-[180px]">
+              {/* 선택된 옵션 버튼 */}
+              <button
+                className="w-full px-4 py-3 bg-white rounded-2xl cursor-pointer flex justify-between items-center"
+                onClick={() => setIsSearchOptionsOpen((prev) => !prev)}
+              >
+                <span>{selectedSort.name}</span>
+                <div>
+                  <img
+                    src="/dropdown.png"
+                    className={`w-4 transform transition-transform duration-200 ${
+                      isSearchOptionsOpen ? "-rotate-90" : "rotate-0"
+                    }`}
+                  />
+                </div>
+              </button>
 
-            {/* 옵션 메뉴 */}
-            {isSearchOptionsOpen && (
-              <div className="absolute top-full left-0 mt-2 w-full bg-white rounded-2xl border border-gray-300 shadow-lg z-10">
-                {sortOptions
-                  .filter((option) => option.key !== selectedSort.key)
-                  .map((option, idx, arr) => (
-                    <button
-                      key={option.key}
-                      className={`
+              {/* 옵션 메뉴 */}
+              {isSearchOptionsOpen && (
+                <div className="absolute top-full left-0 mt-2 w-full bg-white rounded-2xl border border-gray-300 shadow-lg z-10">
+                  {sortOptions
+                    .filter((option) => option.key !== selectedSort.key)
+                    .map((option, idx, arr) => (
+                      <button
+                        key={option.key}
+                        className={`
               w-full px-4 py-3 text-left cursor-pointer flex justify-between items-center
               hover:bg-gray-100
               ${idx === 0 ? "rounded-t-2xl" : ""}
               ${idx === arr.length - 1 ? "rounded-b-2xl" : ""}
             `}
-                      onClick={() => {
-                        setSelectedSort(option);
-                        setIsSearchOptionsOpen(false);
-                      }}
-                    >
-                      <span>{option.name}</span>
-                      <div>
-                        <img src="/dropdown.png" className="w-4" />
-                      </div>
-                    </button>
-                  ))}
-              </div>
-            )}
+                        onClick={() => {
+                          setSelectedSort(option);
+                          setIsSearchOptionsOpen(false);
+                        }}
+                      >
+                        <span>{option.name}</span>
+                        <div>
+                          <img src="/dropdown.png" className="w-4" />
+                        </div>
+                      </button>
+                    ))}
+                </div>
+              )}
+            </div>
           </div>
         </div>
 
