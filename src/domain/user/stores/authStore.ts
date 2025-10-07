ce6332df9111d48568f5e024acff0fb20a85a8df
bestoon <zzzz6747@naver.com>
2025-10-07 22:13:06 +0900

    feat: BaseUser 타입 확장 AuthStore의 AuthState 역시 수정

--- src/domain/user/stores/authStore.ts
+++ src/domain/user/stores/authStore.ts
@@ -1,9 +1,10 @@
-import type { DetailedUser } from "@user/types/user";
+import type { DetailedUser, UserRole } from "@user/types/user";
 import { create } from "zustand";
 import { subscribeWithSelector } from "zustand/middleware";
 
 interface AuthState {
   isLogin: boolean | null;
+  role: UserRole;
   userInfo: DetailedUser | null; // UserType은 유저 정보 타입으로 대체하세요
   setAuth: (userInfo: DetailedUser) => void;
   clearAuth: () => void;
@@ -22,9 +23,11 @@ stateCreator는 저장하고자 하는 상태의 초기값(객체)를 반환
 export const useAuthStore = create<AuthState>()(
   subscribeWithSelector((set) => ({
     isLogin: null,
+    role: null,
     userInfo: null,
-    setAuth: (userInfo) => set({ isLogin: true, userInfo }),
-    clearAuth: () => set({ isLogin: false, userInfo: null }),
+    setAuth: (userInfo) =>
+      set({ isLogin: true, userInfo: userInfo, role: userInfo.role }),
+    clearAuth: () => set({ isLogin: false, role: null, userInfo: null }),
   })),
 );
 /* 리턴값은 useBoundStore 훅으로 매개변수인 선택자(selector)로 선택한 값이 반환된다.
--- src/domain/user/types/user.ts
+++ src/domain/user/types/user.ts
@@ -1,9 +1,13 @@
+// 백엔드의 응답에 맞춘 타입
 export type Auth = {
   accessToken: string;
 };
 
+export type UserRole = "LEARNER" | "EDUCATOR" | null;
+
 export interface BaseUser {
   nickname: string;
+  role: UserRole;
   country: string;
   createTime: string;
 }
