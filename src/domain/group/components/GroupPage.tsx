8ab0f2265c1f93d18df37436baad9a1b55cb29ee
gomDarachi <ttcoristory@naver.com>
2025-10-10 13:56:54 +0900

    refactor: Dropdown컴포넌트로 검새옵션 컴포넌트 합치기

--- src/domain/group/components/GroupPage.tsx
+++ src/domain/group/components/GroupPage.tsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
 import { GroupCard } from "./GroupCard";
+import Dropdown from "@common/components/Dropdown";
 
 const sortOptions = [
   { name: "최신 순", key: "new" },
   { name: "과거 순", key: "old" },
 ];
 function GroupPage() {
-  const [isSearchOptionsOpen, setIsSearchOptionsOpen] =
-    useState<boolean>(false);
   const [selectedSort, setSelectedSort] = useState({
     name: "최신 순",
     key: "new",
@@ -29,48 +28,12 @@ function GroupPage() {
             <img src="/infoIcon.png" alt="새 그룹 만들기" className="w-5 h-5" />
             <span className="font-bold text-rbPrimaryColor">새 반 만들기</span>
           </button>
-          <div className="relative w-[180px]">
-            {/* 선택된 옵션 버튼 */}
-            <button
-              className="w-full px-4 py-3 bg-white rounded-2xl cursor-pointer flex justify-between items-center"
-              onClick={() => setIsSearchOptionsOpen((prev) => !prev)}
-            >
-              <span>{selectedSort.name}</span>
-              <div>
-                <img
-                  src="/dropdown.png"
-                  className={`w-4 transform transition-transform duration-200 ${
-                    isSearchOptionsOpen ? "-rotate-90" : "rotate-0"
-                  }`}
-                />
-              </div>
-            </button>
-
-            {/* 옵션 메뉴 */}
-            {isSearchOptionsOpen && (
-              <div className="absolute top-full left-0 mt-2 w-full bg-white rounded-2xl border border-gray-300 shadow-lg z-10">
-                {sortOptions
-                  .filter((option) => option.key !== selectedSort.key)
-                  .map((option, idx, arr) => (
-                    <button
-                      key={option.key}
-                      className={`
-              w-full px-4 py-3 text-left cursor-pointer flex justify-between items-center
-              hover:bg-gray-100
-              ${idx === 0 ? "rounded-t-2xl" : ""}
-              ${idx === arr.length - 1 ? "rounded-b-2xl" : ""}
-            `}
-                      onClick={() => {
-                        setSelectedSort(option);
-                        setIsSearchOptionsOpen(false);
-                      }}
-                    >
-                      <span>{option.name}</span>
-                    </button>
-                  ))}
-              </div>
-            )}
-          </div>
+          <Dropdown
+            options={sortOptions}
+            selected={selectedSort}
+            onChange={setSelectedSort}
+            width="w-[200px]" // 옵션 (안 넣으면 기본 180px)
+          />
         </div>
       </div>
       <div className="grid grid-cols-4 gap-12">
