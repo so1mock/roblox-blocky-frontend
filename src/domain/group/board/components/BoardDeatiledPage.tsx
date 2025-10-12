a4c87191fbe5ed55bb8b446bc328294916bdf208
gomDarachi <ttcoristory@naver.com>
2025-10-12 17:32:31 +0900

    feat: 게시글 이동 로직 구현

--- src/domain/group/board/components/BoardDeatiledPage.tsx
+++ src/domain/group/board/components/BoardDeatiledPage.tsx
@@ -5,7 +5,13 @@ function BoardDeatiledPage({
   groupId: string;
   boardId: string;
 }) {
-  return <div>BoardDeatiledPage</div>;
+  return (
+    <div className="w-[1600px] mx-auto flex justify-center gap-24">
+      <div className="w-[1200px]">
+        <h1 className="font-bold text-4xl">게시판</h1>
+      </div>
+    </div>
+  );
 }
 
 export default BoardDeatiledPage;
--- src/domain/group/board/components/BoardListItem.tsx
+++ src/domain/group/board/components/BoardListItem.tsx
@@ -1,4 +1,14 @@
+import { useNavigate } from "@tanstack/react-router";
+import { Route as BoardRoute } from "src/routes/student/_mainLayout/group/$groupId/index"; // Route 객체 import
+
 function BoardListItem() {
+  const navigate = useNavigate();
+  const { groupId } = BoardRoute.useParams(); // 현재 그룹 ID
+  const boardId = 1; // api 연결 전 임시 id
+
+  // params의 구조는 라우터 정의에 따라 다름
+  // 예를 들어 route: /student/group/:groupId/board/:boardId
+
   return (
     <div className="whitespace-nowrap py-3 border-solid border-[#DEDEDE] border-b-2 bg-white">
       <div className="inline-block align-middle w-[900px] text-left pl-4 overflow-hidden truncate">
@@ -8,6 +18,7 @@ function BoardListItem() {
           onClick={() => {
             // to do
             // 게시판 상세 페이지로 이동
+            navigate({ to: `/student/group/${groupId}/board/${boardId}` });
           }}
         >
           잼민이는 못 깨는 타워 따라 만들기
