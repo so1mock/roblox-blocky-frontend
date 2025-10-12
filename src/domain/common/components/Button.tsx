51471b0c26dbdda505a6140e32e9bdac22cfca93
gomDarachi <ttcoristory@naver.com>
2025-10-12 17:22:56 +0900

    feat: Button컴포넌트 x, y 크기 설정 매개변수 추가

--- src/domain/common/components/Button.tsx
+++ src/domain/common/components/Button.tsx
@@ -1,15 +1,22 @@
 function Button({
   text,
   handleButtonClick,
+  xSize = 4, // 기본값 px-4
+  ySize = 1, // 기본값 py-1
 }: {
   text: string;
   handleButtonClick: () => void;
+  xSize?: number;
+  ySize?: number;
 }) {
   return (
     <button
       type="button"
       onClick={handleButtonClick}
-      className="bg-rbPrimaryColor rounded-2xl px-4 py-1 cursor-pointer"
+      className="bg-rbPrimaryColor rounded-2xl cursor-pointer"
+      style={{
+        padding: `${ySize * 0.25}rem ${xSize * 0.25}rem`,
+      }}
     >
       <span className="text-white">{text}</span>
     </button>
