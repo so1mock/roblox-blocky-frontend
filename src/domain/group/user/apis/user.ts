91e0aae73707bc29f226ce5040ee05da1bf3fc22
bestoon <zzzz6747@naver.com>
2025-11-05 05:22:44 +0900

    feat: 그룹 회원 목록 조회 api 구현

--- src/domain/group/user/apis/user.ts
+++ src/domain/group/user/apis/user.ts
@@ -1,5 +1,6 @@
 import { api } from "@common/apis/axios";
 import { AxiosError } from "axios";
+import type { GroupMember } from "../types/user";
 
 export const createInviteCode = async (groupUuid: string): Promise<string> => {
   try {
@@ -17,6 +18,20 @@ export const joinGroup = async (inviteCode: string) => {
   try {
     const response = await api.post(`/groups/join`, { inviteCode });
     return 200 <= response.status && response.status < 300;
+  } catch (e) {
+    if (e instanceof AxiosError) {
+      throw e;
+    }
+    throw e;
+  }
+};
+
+export const getGroupMemberList = async (
+  groupUuid: string,
+): Promise<GroupMember[]> => {
+  try {
+    const response = await api.get(`/groups/${groupUuid}/members`);
+    return response.data.members;
   } catch (e) {
     if (e instanceof AxiosError) {
       throw e;
