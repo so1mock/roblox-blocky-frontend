9ea65a7bd0acccc8252832ff19d5a76fa9ec0087
bestoon <zzzz6747@naver.com>
2025-11-05 23:24:11 +0900

    fix: 타입 수정

--- src/domain/group/user/types/user.ts
+++ src/domain/group/user/types/user.ts
@@ -1,17 +1,13 @@
 import type { PlaceSummary } from "@place/types/place";
-import type { UserRole } from "@user/types/user";
 
 export type GroupMember = {
   uuid: string;
   nickname: string;
   profileImageSrc: string | undefined;
-  role: UserRole;
+  role: "OWNER" | "MEMBER";
 };
 
-export type GroupMemberWithPlaces = {
-  uuid: string;
-  nickname: string;
-  profileImageSrc: string | undefined;
-  role: UserRole;
+export type GroupPlaces = {
+  member: GroupMember;
   places: PlaceSummary[];
 };
